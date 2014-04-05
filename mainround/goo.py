f6fc4009b46b7fd56517c9682593ee883bc04aab
Pierre-Olivier Vauboin <povauboin@gmail.com>
2014-04-05 16:08:55 +0200

    from emmanuelle

---
+++ mainround/goo.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+def classer(coord,N):
+    classe=[0]*N;
+    for i in range(0,N):
+       if (coord[i][0]>48.8608): #c'est dans la clase 1 2 3 ou 4
+          if(coord[i][1]<2.0323):
+             classe[i]=1;
+          else:
+             if(coord[i][1]<2.3363):
+                classe[i]=2;
+             else:
+                if(coord[i][1]<2.365):
+                   classe[i]=3;
+                else:
+                   classe[i]=4;
+       else:
+         if(coord[i][1]<2.0323):
+             classe[i]=5;
+         else:
+             if(coord[i][1]<2.3363):
+                classe[i]=6;
+             else:
+                if(coord[i][1]<2.365):
+                   classe[i]=7;
+                else:
+                   classe[i]=8;
+    return classe;
