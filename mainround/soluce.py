df00041a8c20c14687edcb02b1477aabe730a9dd
Pierre-Olivier Vauboin <povauboin@gmail.com>
2014-04-05 12:22:53 +0200

    limit up to 6 steps

--- mainround/soluce.py
+++ mainround/soluce.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+from streetview import *
+
 def create_answer(V):
 
     f = open('answer.txt', 'w')
@@ -10,11 +12,61 @@ def create_answer(V):
         for edge in V[i]:
             f.write('%s\n' % edge) # depart
 
+def choix(s,cout,longueur):
+    n = len(cout[0])
+
+    for j in xrange(n):
+        if (cout[s][j] != -1):
+            return (j,cout[s][j])
+
+
+# deplace une voiture de 1
+def deplace1V(cout,longueur, S, T):
+
+    chemin = [S]
+    c = 0
+    s = S
+
+    steps = 0
+
+    while c < T:
+        (r,t) = choix(s,cout,longueur)
+        chemin.append(r)
+        c += t
+        s = r
+        steps += 1
+        if steps >= 5:
+            break
+
+    return chemin
+
 if '__main__' == __name__:
 
-    C = 8
-    S = 4516
+    # cout = [[0,1,0],
+    #         [1,0,0],
+    #         [1,1,0]]
+    # longueur = [[0,1,0],
+    #             [1,0,0],
+    #             [1,1,0]]
 
-    V = [[S, 4122, 7281, 2751] for i in range(C)]
+    print 'reading map'
+    N, M, T, C, S, coord, cost, length = readfile('paris_54000.txt')
 
+    print 'N, M, T, C, S'
+    print N, M, T, C, S
+
+    # T = 3
+    # S = 0
+
+    print 'choosing path'
+    chemin =  deplace1V(cost,length,S,50)
+    # print chemin
+
+    # C = 8
+    # S = 4516
+
+    # V = [[S, 4122, 7281, 2751] for i in range(C)]
+    V = [chemin for i in range(C)]
+
+    print 'writing answer'
     create_answer(V)
