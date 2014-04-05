ab1e8d0e4fdfe7140c3217ebc02d7f489525a83d
Pierre-Olivier Vauboin <povauboin@gmail.com>
2014-04-05 11:49:17 +0200

    create better solution by hand

--- mainround/soluce.py
+++ mainround/soluce.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-def create_answer():
-
-    C = 8
-    S = 4516
-
-    V = [[S] for i in range(C)]
+def create_answer(V):
 
     f = open('answer.txt', 'w')
-    f.write('%s\n' % C)
-    for i in range(C):
+    f.write('%s\n' % len(V))
+    for i in range(len(V)):
         f.write('%s\n' % len(V[i])) # nb intersections visitees par ci
-        # f.write('%s\n' % S) # depart
         for edge in V[i]:
             f.write('%s\n' % edge) # depart
 
 if '__main__' == __name__:
 
-    create_answer()
+    C = 8
+    S = 4516
+
+    V = [[S, 4122, 7281, 2751] for i in range(C)]
+
+    create_answer(V)
