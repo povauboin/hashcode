80cec45a2c6ef6725b3ce8a5e45d7e916ee59a4d
Pierre-Olivier Vauboin <povauboin@gmail.com>
2014-04-05 16:51:14 +0200

    fix typo in gps

--- mainround/goo.py
+++ mainround/goo.py
@@ -5,7 +5,7 @@ def classer(coord,N):
     classe=[0]*N;
     for i in range(0,N):
        if (coord[i][0]>48.8608): #c'est dans la clase 1 2 3 ou 4
-          if(coord[i][1]<2.0323):
+          if(coord[i][1]<2.3023):
              classe[i]=1;
           else:
              if(coord[i][1]<2.3363):
