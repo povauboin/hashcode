745de2ddc6f5c7a4d66f7f9f104ef835698e333c
Pierre-Olivier Vauboin <povauboin@gmail.com>
2014-04-05 14:03:38 +0200

    use dijkstra to get to firs position

--- mainround/soluce.py
+++ mainround/soluce.py
@@ -33,15 +33,71 @@ def choix(s,cout,longueur):
 
     return (J,cout[s][J],longueur)
 
+# from http://stackoverflow.com/questions/4997851/python-dijkstra-algorithm
+def dijkstra(net, s, t):
+    # sanity check
+    if s == t:
+        return "The start and terminal nodes are the same. Minimum distance is 0."
+    if net.has_key(s)==False:
+        return "There is no start node called " + str(s) + "."
+    if net.has_key(t)==False:
+        return "There is no terminal node called " + str(t) + "."
+    # create a labels dictionary
+    labels={}
+    # record whether a label was updated
+    order={}
+    # populate an initial labels dictionary
+    for i in net.keys():
+        if i == s: labels[i] = 0 # shortest distance form s to s is 0
+        else: labels[i] = float("inf") # initial labels are infinity
+    from copy import copy
+    drop1 = copy(labels) # used for looping
+    ## begin algorithm
+    while len(drop1) > 0:
+        # find the key with the lowest label
+        minNode = min(drop1, key = drop1.get) #minNode is the node with the smallest label
+        # update labels for nodes that are connected to minNode
+        for i in net[minNode]:
+            if labels[i] > (labels[minNode] + net[minNode][i]):
+                labels[i] = labels[minNode] + net[minNode][i]
+                drop1[i] = labels[minNode] + net[minNode][i]
+                order[i] = minNode
+        del drop1[minNode] # once a node has been visited, it's excluded from drop1
+    ## end algorithm
+    # print shortest path
+    temp = copy(t)
+    rpath = []
+    path = []
+    while 1:
+        rpath.append(temp)
+        if order.has_key(temp): temp = order[temp]
+        else: return "There is no path from " + str(s) + " to " + str(t) + "."
+        if temp == s:
+            rpath.append(temp)
+            break
+    for j in range(len(rpath)-1,-1,-1):
+        path.append(rpath[j])
+
+    return path, labels[t]
+    # return "The shortest path from " + s + " to " + t + " is " + str(path) + ". Minimum distance is " + str(labels[t]) + "."
+
+# Given a large random network find the shortest path from '0' to '5'
+# print dijkstra(net=randNet(), s='0', t='5')
 
 # deplace une voiture de 1
-def deplace1V(cout,longueur, S, T):
+def deplace1V(cout,longueur, S, T, first):
 
-    chemin = [S]
+    chemin = []
     c = 0
     s = S
 
     steps = 0
+
+    path,t = dijkstra(cout, S, first)
+    c += t
+    for p in path:
+        chemin.append(p)
+        s = p
 
     while c < T:
         (r,t,longueur) = choix(s,cout,longueur)
@@ -81,10 +137,12 @@ if '__main__' == __name__:
     # C = 8
     # S = 4516
 
+    start = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000]
+
     # V = [[S, 4122, 7281, 2751] for i in range(C)]
     V = []
     for i in range(C):
-        (chemin,length) = deplace1V(cost,length,S,T)
+        (chemin,length) = deplace1V(cost,length,S,T, start[i])
         V.append(chemin)
 
     print 'writing answer'
