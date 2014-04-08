e4f881c2f91568a9621bb98f7d7b63ad27e1dcc1
Pierre-Olivier Vauboin <povauboin@gmail.com>
2014-04-08 04:16:38 +0200

    look 3 nodes further

--- mainround/soluce.py
+++ mainround/soluce.py
@@ -1,18 +1,52 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import copy
 from streetview import *
 from goo import *
 import random
 
 def create_answer(V):
 
-    f = open('answer3.txt', 'w')
+    f = open('answer.txt', 'w')
     f.write('%s\n' % len(V))
     for i in range(len(V)):
         f.write('%s\n' % len(V[i])) # nb intersections visitees par ci
         for edge in V[i]:
             f.write('%s\n' % edge) # depart
+
+def look_ahead(s, cout, longueur, chemin, cost, score, depth):
+
+    # print ('look_ahead, s: %s, depth: %s' % (s, depth))
+
+    if depth <= 0:
+        return (score, cost, chemin)
+
+    J = 0
+    best_score = score
+
+    # longueur_copy = copy.deepcopy(longueur)
+
+    for j in cout[s].keys():
+        ratio = float(longueur[s][j]) / cout[s][j]
+
+        longueur[s][j] /= 1.85
+        if j in longueur and s in longueur[j]:
+            longueur[j][s] /= 1.85
+
+        (new_score, new_cost, new_path) = look_ahead(j, cout, longueur, chemin + [j], cost + [cout[s][j]], score + [ratio], depth-1)
+
+        longueur[s][j] *= 1.85
+        if j in longueur and s in longueur[j]:
+            longueur[j][s] *= 1.85
+
+        if sum(new_score) >= sum(best_score):
+            J = j
+            best_score = new_score
+            best_path = new_path
+            best_cost = new_cost
+
+    return (best_score, best_cost, best_path)
 
 def choix(s,cout,longueur, chemin, visited, classes, car_num):
     # n = len(cout[0])
@@ -20,7 +54,7 @@ def choix(s,cout,longueur, chemin, visited, classes, car_num):
     max_ratio = 0
     J = 0
 
-    choose next node only from neighbors in car sector
+    # choose next node only from neighbors in car sector
     neighbors = cout[s].keys()
     neighbors_in_sector = [node for node in neighbors if classes[node] == car_num]
     if False:
@@ -37,12 +71,7 @@ def choix(s,cout,longueur, chemin, visited, classes, car_num):
             max_ratio = ratio
 
     # if no neighbors is in sector
-    for j in cout[s].keys():
-        ratio = float(longueur[s][j]) / cout[s][j]
-        # for k in cout[j].keys():
-        if (ratio >= max_ratio):
-            J = j
-            max_ratio = ratio
+    look_ahead(s, cout, longueur, [], 0, 2)
 
     # print ('min ratio:', min(ratios))
     # print ('max ratio:', max(ratios))
@@ -74,8 +103,7 @@ def dijkstra(net, s, t):
     for i in net.keys():
         if i == s: labels[i] = 0 # shortest distance form s to s is 0
         else: labels[i] = float("inf") # initial labels are infinity
-    from copy import copy
-    drop1 = copy(labels) # used for looping
+    drop1 = copy.copy(labels) # used for looping
     ## begin algorithm
     while len(drop1) > 0:
         # find the key with the lowest label
@@ -89,7 +117,7 @@ def dijkstra(net, s, t):
         del drop1[minNode] # once a node has been visited, it's excluded from drop1
     ## end algorithm
     # print shortest path
-    temp = copy(t)
+    temp = copy.copy(t)
     rpath = []
     path = []
     while 1:
@@ -127,44 +155,22 @@ def deplace1V(cout,longueur, S, T, first, visited, classes, car_num):
         chemin.append(p)
         s = p
 
-    while c < T:
-        if all(loop) and False:
-            notvisited = [node for node in range(N) if not visited[node]]
-            # print 'len notvisited', len(notvisited)
-            # print ('car_num', car_num)
-            # notvisited_car = [node for node in notvisited if (classes[node] == car_num)]
-            notvisited_car = []
-            for node in notvisited:
-                # print 'node', node
-                # print ('classe node', classes[node])
-                if (classes[node] == car_num):
-                    notvisited_car.append(node)
-            if notvisited_car == []:
-                print ('all visited for this car')
-                print 'car_num', car_num
-                notvisited_car = notvisited
-            nextfirst = random.choice(notvisited_car)
-            path, t = dijkstra(cout, s, nextfirst)
-            c += t
-            for p in path:
-                chemin.append(p)
-            s = p
-            loop = [False] * 10
-        else:
-            (r,t,longueur,ratio) = choix(s,cout,longueur, chemin, visited, classes, car_num)
-            counter += 1
-            # if not classes[r] == car_num:
-            #     loop = [True] * 10
-            # else:
-            loop[counter % 10] = (ratio < limit)
-            if (c + t) <= T:
-                chemin.append(r)
-                visited[r] = 1
-                s = r
-        c += t
-        # steps += 1
-        # if steps >= 500:
-        #     break
+    while c <= T:
+        (new_score, new_cost, new_path) = look_ahead(s, cout, longueur, [], [], [], 3)
+        print (new_score, new_cost, new_path)
+        for i in xrange(len(new_path)):
+            # print ('add %s' % new_path[i])
+            # print 'c', c
+            # print ('len chemin %s' % len(chemin))
+            longueur[s][new_path[i]] /= 1.85
+            if new_path[i] in longueur and s in longueur[new_path[i]]:
+                longueur[new_path[i]][s] /= 1.85
+            c += new_cost[i]
+            if c <= T:
+                chemin.append(new_path[i])
+                s = new_path[i]
+            else:
+                break
 
     print 'c', c
     print 'car_num', car_num
